3bbc39a0c0604e2134b31fa9c225b5e54a000882
Joshua Szmajda <josh@optoro.com>
2014-12-03 07:35:49 -0500

    using applicative style

--- src/Raskell/Parser/Whitespace.hs
+++ src/Raskell/Parser/Whitespace.hs
@@ -7,12 +7,10 @@ import Text.Parsec (many)
 import Text.Parsec.String (Parser)
 import Text.Parsec.Char
 import Control.Monad (void)
+import Control.Applicative ((<*))
 
 whitespace :: Parser ()
 whitespace = void $ many $ oneOf " \t"
 
 lexeme :: Parser a -> Parser a
-lexeme p = do
-  x <- p
-  whitespace
-  return x
+lexeme p = p <* whitespace
