ca13aff1b797af55e20e5010089e39aed6946070
Joshua Szmajda <josh@optoro.com>
2015-10-31 12:51:48 -0400

    something very basic puts's now

--- src/Raskell/PrimImpl.hs
+++ src/Raskell/PrimImpl.hs
@@ -9,7 +9,8 @@ import qualified Raskell.PrimImpl.FixNum as ImplFixNum
 
 -- hmmm this should return an IO action but I'm not sure how yet
 invokeMethodIO :: ProgramContext -> Rb.RbObject -> Rb.MethodName -> [Rb.RbObject] -> IO Rb.RbObject
-invokeMethodIO _ _ = undefined
+invokeMethodIO ctxt _ "puts" args = putStrLn (show args) >> return Rb.Null
+invokeMethodIO _ _ _ _ = undefined
 
 invokeMethod :: ProgramContext -> Rb.RbObject -> Rb.MethodName -> [Rb.RbObject] -> Rb.RbObject
 invokeMethod ctxt fixnum@(Rb.FixNum _) = ImplFixNum.invokeMethod ctxt fixnum
